d8523ec37e077077d1634d68efe96c9ec112f4a7
fatihturker <fatih.turker@optiva.com>
2021-09-17 10:56:19 +0300

    Upgrade common

--- app/queue/public-mail.queue.ts
+++ app/queue/public-mail.queue.ts
@@ -3,12 +3,14 @@ import { PUBLIC_MAIL_MQ_ACTION } from '../../app.constant';
 
 export class PublicMailQueue {
   constructor(private mq: MessageQueueProvider, private channel: string) {}
-  onMessage = (msg: any) => {
+  onMessage = async(msg: any) => {
     var msgStr = msg.content.toString();
     var msgObj = JSON.parse(msgStr);
     if (msgObj.action === PUBLIC_MAIL_MQ_ACTION) {
       console.log('Message Received: ', msgObj);
-      this.mq.acknowledge(this.channel, msg);
+      // Manual ACK should be fixed
+      // await this.mq.acknowledge(this.channel, msg);
+      // console.log('Message Processed: ', msgObj);
     }
   };
 }
