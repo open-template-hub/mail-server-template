45b7d51f8ce8a90bf4220cebb94d71135890be14
Furkan Yavuz <furknyavuz@gmail.com>
2021-09-18 17:36:32 +0300

    update common and mail consumer

--- app/consumer/mail-queue.consumer.ts
+++ app/consumer/mail-queue.consumer.ts
@@ -1,24 +1,39 @@
-import { Actions, MessageQueueProvider } from '@open-template-hub/common';
+import { MailActionType } from '@open-template-hub/common/lib/action/mail.action';
 
 export class MailQueueConsumer {
-  constructor(private channel: any) {}
 
-  onMessage = async (msg: any) => {
-    if (msg !== null) {
+  constructor( private channel: any ) {
+  }
+
+  onMessage = async ( msg: any ) => {
+    if ( msg !== null ) {
       const msgStr = msg.content.toString();
-      const msgObj = JSON.parse(msgStr);
-      if (msgObj.action === Actions.MailActions.CONTACT_US) {
+      const msgObj = JSON.parse( msgStr );
+
+      const message: MailActionType = msgObj.message;
+
+      console.log( msgObj.message );
+      console.log( message );
+
+      if ( message.contactUs ) {
+        let requeue = false;
+
         try {
-          console.log('Message Received: ', msgObj);
-          await this.channel.ack(msg);
-          console.log('Message Processed: ', msgObj);
-        } catch (e) {
-          console.log('Error while processing message: ', msgObj);
-          console.log('Stacktrace: ', e);
+          console.log( 'Message Received with deliveryTag: ' + msg.fields.deliveryTag, msgObj );
+
+          // TODO: send contact us mail
+
+          await this.channel.ack( msg );
+          console.log( 'Message Processed with deliveryTag: ' + msg.fields.deliveryTag, msgObj );
+        } catch ( e ) {
+          console.log( 'Error with processing deliveryTag: ' + msg.fields.deliveryTag, msgObj, e );
+
+          // Decide requeue in the error handling
+          this.channel.nack( msg, false, requeue );
         }
       } else {
-        console.log('Message will be rejected: ', msgObj);
-        //await this.mq.rejectAck( this.channel, msg, false );
+        console.log( 'Message will be rejected: ', msgObj );
+        this.channel.reject( msg, false );
       }
     }
   };
