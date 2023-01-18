6894bf5142090aee4cfe96f5270b7311c21ee26a
Fatih Turker <fatihturker35@gmail.com>
2023-01-18 17:03:01 +0300

    Implement DLQ logic

--- app/consumer/mail-queue.consumer.ts
+++ app/consumer/mail-queue.consumer.ts
@@ -1,9 +1,14 @@
-import { ContextArgs, MongoDbProvider, QueueConsumer, } from '@open-template-hub/common';
+import {
+  ContextArgs,
+  MessageQueueChannelType,
+  MongoDbProvider,
+  QueueConsumer,
+  QueueMessage,
+} from '@open-template-hub/common';
 import { MailActionType } from '@open-template-hub/common/lib/action/mail.action';
 import { MailController } from '../controller/mail.controller';
 
 export class MailQueueConsumer implements QueueConsumer {
-
   private channel: any;
   private ctxArgs: ContextArgs = {} as ContextArgs;
   private mailController: MailController;
@@ -12,16 +17,16 @@ export class MailQueueConsumer implements QueueConsumer {
     this.mailController = new MailController();
   }
 
-  init = ( channel: string, ctxArgs: ContextArgs ) => {
+  init = (channel: string, ctxArgs: ContextArgs) => {
     this.channel = channel;
     this.ctxArgs = ctxArgs;
     return this;
   };
 
-  onMessage = async ( msg: any ) => {
-    if ( msg !== null ) {
+  onMessage = async (msg: any) => {
+    if (msg !== null) {
       const msgStr = msg.content.toString();
-      const msgObj = JSON.parse( msgStr );
+      const msgObj = JSON.parse(msgStr);
 
       const message: MailActionType = msgObj.message;
 
@@ -33,64 +38,87 @@ export class MailQueueConsumer implements QueueConsumer {
       let params: any | undefined;
 
       if (
-          message &&
-          message?.mailType &&
-          Object.keys( message.mailType )?.length > 0
+        message &&
+        message?.mailType &&
+        Object.keys(message.mailType)?.length > 0
       ) {
-        key = Object.keys( message.mailType )[ 0 ];
-        params = ( message.mailType as any )[ key ]?.params;
+        key = Object.keys(message.mailType)[0];
+        params = (message.mailType as any)[key]?.params;
         to = params?.email;
       } else {
-        console.log( 'Message will be rejected: ', msgObj );
-        this.channel.reject( msg, false );
+        console.log('Message will be rejected: ', msgObj);
+        this.channel.reject(msg, false);
         return;
       }
 
-      if ( key && params ) {
+      if (key && params) {
         let hook = async () => {
           await this.mailController.sendMail(
-              this.ctxArgs.mongodb_provider as MongoDbProvider,
-              key as string,
-              message.language,
-              to as string,
-              params
+            this.ctxArgs.mongodb_provider as MongoDbProvider,
+            key as string,
+            message.language,
+            to as string,
+            params
           );
         };
 
-        await this.operate( msg, msgObj, requeue, hook );
+        await this.operate(msg, msgObj, requeue, hook);
       } else {
-        console.log( 'Message will be rejected: ', msgObj );
-        this.channel.reject( msg, false );
+        console.log('Message will be rejected: ', msgObj);
+        this.channel.reject(msg, false);
         return;
       }
     }
   };
 
   private operate = async (
-      msg: any,
-      msgObj: any,
-      requeue: boolean,
-      hook: Function
+    msg: any,
+    msgObj: any,
+    requeue: boolean,
+    hook: Function
   ) => {
     try {
       console.log(
-          'Message Received with deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj
+        'Message Received with deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj
       );
       await hook();
-      await this.channel.ack( msg );
+      await this.channel.ack(msg);
       console.log(
-          'Message Processed with deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj
+        'Message Processed with deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj
       );
-    } catch ( e ) {
+    } catch (e) {
       console.log(
-          'Error with processing deliveryTag: ' + msg.fields.deliveryTag,
-          msgObj,
-          e
+        'Error with processing deliveryTag: ' + msg.fields.deliveryTag,
+        msgObj,
+        e
       );
 
-      this.channel.nack( msg, false, requeue );
+      await this.moveToDLQ(msg, requeue);
+    }
+  };
+
+  private moveToDLQ = async (msg: any, requeue: boolean) => {
+    try {
+      const orchestrationChannelTag =
+        this.ctxArgs.envArgs.mqArgs?.orchestrationServerMessageQueueChannel;
+
+      const message = {
+        sender: MessageQueueChannelType.MAIL,
+        receiver: MessageQueueChannelType.DLQ,
+        message: msg,
+      } as QueueMessage;
+
+      await this.ctxArgs.message_queue_provider?.publish(
+        message,
+        orchestrationChannelTag as string
+      );
+
+      this.channel.reject(msg, false);
+    } catch (e) {
+      console.log('Error while moving message to DLQ: ', msg);
+      this.channel.nack(msg, false, requeue);
     }
   };
 }
